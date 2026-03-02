68039865e84e929b427ec706bab21e8b0b433481
unknown <johnnycasteloutlook.esgit config --global user.email johnnycastelo^Cutlook.es.>
2026-03-02 11:35:30 -0600

    Seeder para asignar roles a usuarios

---
+++ database/seeders/AssignRolesSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class AssignRolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //Usamos ID institucional
+        $adminIds = [210318,9520];
+        $assistenteIds = [38960,247395];
+        $jefeIds = [9393,98901,99606,89142];
+
+        $roles = Role::pluck('name')->toArray();
+
+        foreach (User::all() as $user) {
+            
+            if (in_array($user->id_uaa, $adminIds)) {
+                $user->syncRoles('admin');
+            } elseif (in_array($user->id_uaa, $assistenteIds)) {
+                $user->syncRoles('asistente');
+            } elseif (in_array($user->id_uaa, $jefeIds)) {
+                $user->syncRoles('jefe');
+            } else {
+                $user->syncRoles('empleado');
+            }
+        }
+    }
+}
